cd75568a3a4735844a8a8f65b5b19e9bf9d948cd
mnaufalrifqir <mnaufalrifqir@gmail.com>
2023-05-08 02:10:38 +0700

    model: add validate task model

--- model/task.go
+++ model/task.go
@@ -9,10 +9,10 @@ import (
 type Task struct {
 	gorm.Model
 	UserID       uint
-	Name         string    `json:"name" form:"name"`
+	Name         string    `json:"name" form:"name" validate:"required"`
 	Status       string    `json:"status" gorm:"type:enum('finished', 'unfinished');default:'unfinished';not_null"`
-	DueDate      time.Time `json:"due_date" form:"due_date"`
+	DueDate      time.Time `json:"due_date" form:"due_date" validate:"required"`
 	TaskPriority string    `json:"task_priority" gorm:"type:enum('high', 'medium', 'low');default:'low';not_null"`
 	Description  string    `json:"description" form:"description"`
-	User         User      `json:"-" gorm:"foreignKey:UserID"`
+	User         User      `json:"-" gorm:"foreignKey:UserID" validate:"-"`
 }
